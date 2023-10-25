bd4da602ea06fcab780c582ae6528c6ebe031027
Rajmandal17 <Rajmandal147@gmail.com>
2023-10-25 15:31:15 +0530

    order api

--- src/main/java/com/gitbitex/openapi/model/TradeDto.java
+++ src/main/java/com/gitbitex/openapi/model/TradeDto.java
@@ -7,8 +7,31 @@ import lombok.Setter;
 @Setter
 public class TradeDto {
     private long sequence;
-    private String time;
+
     private String price;
+
+    private String takeruserId;
+    private String makeruserId;
+    private String time;
+    private String tradeId;
+    private String takerOrderId;
+    private String makerOrderId;
+    private String logOffset;
     private String size;
     private String side;
+    private String makerfunds;
+    private String makerfillFees;
+    private String makerfilledSize;
+    private String makerexecutedValue;
+    private String makerstatus;
+    private String takerfunds;
+    private String takerfillFees;
+    private String takerfilledSize;
+    private String takerexecutedValue;
+    private String takerstatus;
+
+
+
+
+
 }
